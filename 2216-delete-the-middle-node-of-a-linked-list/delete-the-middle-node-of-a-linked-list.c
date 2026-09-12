b05f6f795ece3ed38b222de4673afcd32a0bc4da
Code with AYush <gairolaayush777@gmail.com>
2026-09-12 21:10:05 +0530

    Time: 4 ms (33.68%) | Memory: 83.1 MB (57.70%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.c
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.c
@@ -6,19 +6,15 @@
  * };
  */
 struct ListNode* deleteMiddle(struct ListNode* head) {
+    struct ListNode*slow=head;
+    struct ListNode*fast=head->next;
     if(head==NULL || head->next==NULL){
         return NULL;
     }
-    struct ListNode* temp=head;
-    int count=0;
-    while(temp!=NULL){
-        count++;
-        temp=temp->next;
+    while(fast->next!=NULL && fast->next->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
     }
-    temp=head;
-    for(int i=0;i<count/2-1;i++){
-        temp=temp->next;
-    }
-    temp->next=temp->next->next;
+    slow->next=slow->next->next;
     return head;
 }
